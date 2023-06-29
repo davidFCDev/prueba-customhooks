a42deeffc9a921bc25895f754295406a436808fd
David Fernández <davidfernandezcomesana@gmail.com>
2023-06-29 13:53:06 +0200

    añadido estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import React, { useEffect, useState } from 'react'
+import { getRandomFact } from './services/facts'
+import './App.css'
 
-const CAT_RANDOM_IMAGE = 'https://cataas.com/cat/says/hello'
+// const CAT_RANDOM_IMAGE = 'https://cataas.com/cat/says/hello'
 
 export default function App () {
   const [fact, setFact] = useState('')
-  const [image, setImage] = useState('')
+  // const [image, setImage] = useState('')
 
   useEffect(() => {
-
+    getRandomFact().then(setFact)
   }, [])
 
+  const handleClick = () => {
+    getRandomFact().then(setFact)
+  }
+
   return (
-    <div>
+    <main>
       <h1>App de gatos</h1>
-      {fact && <p>{fact}</p>}
-      {image && <img src={image} alt='Imagen aleatoria de la api de gatos' />}
-    </div>
+      <button onClick={handleClick}>New fact</button>
+      <p>{fact}</p>
+      {/* {image && <img src={image} alt='Imagen aleatoria de la api de gatos' />} */}
+    </main>
   )
 }
