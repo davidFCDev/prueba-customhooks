c3053186ba7ccf3bcf51c10d79bfd282e87cb6d0
David Fernández <davidfernandezcomesana@gmail.com>
2023-06-29 13:29:21 +0200

    añadida llamada api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function App() {
+const CAT_RANDOM_IMAGE = 'https://cataas.com/cat/says/hello'
+
+export default function App () {
+  const [fact, setFact] = useState('')
+  const [image, setImage] = useState('')
+
+  useEffect(() => {
+
+  }, [])
+
   return (
-    <div>Hola mundo!</div>
+    <div>
+      <h1>App de gatos</h1>
+      {fact && <p>{fact}</p>}
+      {image && <img src={image} alt='Imagen aleatoria de la api de gatos' />}
+    </div>
   )
 }
